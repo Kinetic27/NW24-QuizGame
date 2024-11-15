37c404043af3dacb5cbe6c66f22ece8f3b7c72ce
Kinetic27 <aheui@kakao.com>
2024-11-15 19:35:57 +0900

    Add comment

--- app/src/main/java/kr/co/gachon/kinetic27/nw24_quizgame/MainActivity.java
+++ app/src/main/java/kr/co/gachon/kinetic27/nw24_quizgame/MainActivity.java
@@ -22,16 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
+        // install splash screen for main activity
         SplashScreen.installSplashScreen(this);
 
+
+        // set layout
         setContentView(R.layout.activity_main);
 
+        // default setting for screen
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // read ip and port from dat file
         String[] ipAndPort = readIpAndPortFromFile("server_info.dat");
 
         String ip = ipAndPort[0];
@@ -40,26 +45,37 @@ public class MainActivity extends AppCompatActivity {
         TextView ip_text = findViewById(R.id.ip_text);
         TextView port_text = findViewById(R.id.port_text);
 
+        // set default text in to Edittext
         ip_text.setText(ip);
         port_text.setText(String.valueOf(port));
 
         Button startButton = findViewById(R.id.startButton);
 
+        // add click listener
         startButton.setOnClickListener(v -> {
+            // intent to QuizActivity
             Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+
+            // put server ip and port
             intent.putExtra("ip", ip_text.getText().toString());
             intent.putExtra("port", Integer.parseInt(port_text.getText().toString()));
+
+            // start activity
             startActivity(intent);
         });
     }
 
+    // read ip and port from dat file
     public String[] readIpAndPortFromFile(String filePath) {
+        // buffer reader with try-catch
         try (BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open(filePath)))) {
+            // read ip and port line by line
             String ip = br.readLine();
             String port = br.readLine();
 
             return new String[]{ip, port};
         } catch (IOException | NumberFormatException e) {
+            // if error, return default ip and port
             return new String[]{"localhost", "1234"};
         }
     }
--- app/src/main/java/kr/co/gachon/kinetic27/nw24_quizgame/QuizActivity.java
+++ app/src/main/java/kr/co/gachon/kinetic27/nw24_quizgame/QuizActivity.java
@@ -41,18 +41,23 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        // set layout
         setContentView(R.layout.activity_quiz);
 
+        // default setting for screen
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.quiz_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // load views
         quizText = findViewById(R.id.quiz_text);
         quizLengthText = findViewById(R.id.quiz_text_size);
         answerText = findViewById(R.id.answer_text);
 
+        // get intent from main activity
         Intent intent = getIntent();
         String ip = intent.getStringExtra("ip");
         int port = intent.getIntExtra("port", 1234);
@@ -64,27 +69,37 @@ public class QuizActivity extends AppCompatActivity {
         // connect to server
         openSocket(ip, port);
 
+        // add submit listener
         submitButton.setOnClickListener(v -> {
+            // get text with trim()
             String answer = answerText.getText().toString().trim();
             Log.i("answer", answer);
 
+            // if answer is empty, nothing works
             if (!answer.isEmpty()) {
+                // encode answer with URL encoder
                 String encoded_answer = URLEncoder.encode(answer, StandardCharsets.UTF_8);
                 Log.i("answer", encoded_answer);
 
+                // hide keyboard
                 InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
                 manager.hideSoftInputFromWindow(Objects.requireNonNull(getCurrentFocus()).getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 
+                // send answer to server in new thread
                 new Thread(() -> {
                     out.println("q_answer:" + encoded_answer);
 
+                    // read correctness and new quiz in server
                     readLineFromServer();
                 }).start();
             }
         });
 
+        // add enter key listener to convenient
         answerText.setOnKeyListener((v, keyCode, event) -> {
             if (keyCode == KeyEvent.KEYCODE_ENTER) {
+
+                // do submit
                 submitButton.callOnClick();
             }
 
@@ -92,6 +107,7 @@ public class QuizActivity extends AppCompatActivity {
         });
     }
 
+    // connect to server
     public void openSocket(String ip, int port) {
         new Thread(() -> {
             try {
@@ -101,25 +117,31 @@ public class QuizActivity extends AppCompatActivity {
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 out = new PrintWriter(clientSocket.getOutputStream(), true);
 
+                // read line from server
                 readLineFromServer();
             } catch (IOException e) {
                 Log.e("QuizActivity", Objects.requireNonNull(e.getMessage()));
                 Log.e("QuizActivity", "Server Not Ready");
 
+                // if server not ready, finish activity and show toast msg
                 runOnUiThread(() -> Toast.makeText(this, "Server Not Ready", Toast.LENGTH_SHORT).show());
                 finish();
             }
         }).start();
     }
 
+    // read line from server
     @SuppressLint("SetTextI18n")
     public void readLineFromServer() {
         try {
             String serverMsg = in.readLine();
 
+            // if server send quiz
             if (serverMsg.startsWith("quiz:")) {
+                // split quiz with :
                 String[] quiz = serverMsg.split(":");
 
+                // set quiz and quiz length text
                 runOnUiThread(() -> {
                     quizText.setText(quiz[1]);
                     quizLengthText.setText(quiz[2] + "글자");
@@ -130,6 +152,8 @@ public class QuizActivity extends AppCompatActivity {
             if (serverMsg.startsWith("answer:")) {
                 String[] cmd = serverMsg.split(":");
 
+
+                // check answer is correct and show snackbar with correctness
                 runOnUiThread(() -> {
                     if (Objects.equals(cmd[1], "correct")) {
                         Snackbar.make(findViewById(R.id.quiz_main), "정답입니다!", Snackbar.LENGTH_SHORT).show();
@@ -137,24 +161,29 @@ public class QuizActivity extends AppCompatActivity {
                         Snackbar.make(findViewById(R.id.quiz_main), "오답입니다!", Snackbar.LENGTH_SHORT).show();
                     }
 
+                    // set answer text to empty
                     answerText.setText("");
                 });
 
-                // call recursively
+                // call recursively for read new quiz
                 readLineFromServer();
             }
 
             // if server message is show score
             if (serverMsg.startsWith("score:")) {
+                // get score from server message and split it with :
                 int score = Integer.parseInt(serverMsg.split(":")[1]);
 
                 runOnUiThread(() -> {
+                    // disable submit button and answer text while snackbar show
                     findViewById(R.id.submit_button).setEnabled(false);
                     answerText.setEnabled(false);
 
+                    // show score
                     quizText.setText(score + "점 입니다!");
                     quizLengthText.setText("수고하셨습니다.");
 
+                    // show snackbar with score and finish activity
                     new Handler().postDelayed(() -> {
                         Snackbar.make(findViewById(R.id.quiz_main),
                                 "최종 점수는 " + score + "점 입니다.", Snackbar.LENGTH_SHORT
@@ -168,6 +197,7 @@ public class QuizActivity extends AppCompatActivity {
             Log.e("QuizActivity", Objects.requireNonNull(e.getMessage()));
             Log.e("QuizActivity", "Server Error");
 
+            // if server error, finish activity and show toast msg
             runOnUiThread(() -> Toast.makeText(this, "Server Error", Toast.LENGTH_SHORT).show());
             QuizActivity.this.finish();
         }
